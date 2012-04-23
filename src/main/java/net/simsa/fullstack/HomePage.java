740c08c838408bcbd39f66c99ce0b3156e2e2b94
Jenny Brown <jennybrown8@gmail.com>
2012-04-22 19:36:29 -0500

    Added wicket-cdi and enabled cdi injection.

--- src/main/java/net/simsa/fullstack/HomePage.java
+++ src/main/java/net/simsa/fullstack/HomePage.java
@@ -1,14 +1,21 @@
 package net.simsa.fullstack;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import javax.inject.Inject;
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-    public HomePage(final PageParameters parameters) {
+	@Inject
+	Clock clock;
+
+	public HomePage(final PageParameters parameters)
+	{
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-        // TODO Add your page's components here
-    }
+		add(new Label("clock", new PropertyModel(this, "clock.time")));
+	}
 }
--- src/main/java/net/simsa/fullstack/WicketApplication.java
+++ src/main/java/net/simsa/fullstack/WicketApplication.java
@@ -1,14 +1,19 @@
 package net.simsa.fullstack;
 
+import javax.enterprise.inject.spi.BeanManager;
+
+import net.ftlines.wicket.cdi.CdiConfiguration;
+
 import org.apache.wicket.protocol.http.WebApplication;
+import org.jboss.weld.environment.servlet.Listener;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this application without deploying, run the
+ * Start class.
  * 
  * @see net.simsa..Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{    	
+public class WicketApplication extends WebApplication {
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
@@ -26,6 +31,12 @@ public class WicketApplication extends WebApplication
 	{
 		super.init();
 
-		// add your configuration here
+		BeanManager manager = (BeanManager) getServletContext().getAttribute(Listener.BEAN_MANAGER_ATTRIBUTE_NAME);
+		new CdiConfiguration(manager).configure(this);
+		
+		// in application servers you can retrieve the bean manager from JNDI:
+
+		// manager = (BeanManager)new
+		// InitialContext().lookup("java:comp/BeanManager");
 	}
 }
