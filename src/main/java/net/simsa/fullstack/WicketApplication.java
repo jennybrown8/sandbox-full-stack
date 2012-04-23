35352e67f4537a1b81011b29f4211bc4881ec2cf
Jenny Brown <jennybrown8@gmail.com>
2012-04-22 19:15:26 -0500

    fixed project name, packages

---
+++ src/main/java/net/simsa/fullstack/WicketApplication.java
@@ -0,0 +1,31 @@
+package net.simsa.fullstack;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see net.simsa..Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
